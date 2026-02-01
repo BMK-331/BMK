ef593abc3f022ec4d00ec447b3b79f7e59b8ed41
Kartikeya Ojha <kartikeyaojha001@gmail.com>
2026-02-01 22:10:27 +0530

    feat: add trpc routers for ai operations

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,30 @@
 import { z } from "zod";
 import prisma from "../../lib/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
-  getWorkflows: protectedProcedure.query(() => prisma.workflow.findMany()),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
 
-  createWorkflow: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(({ input }) => prisma.workflow.create({ data: { name: input.name } })),
+  createWorkflow: protectedProcedure.mutation(() => {
+    return prisma.workflow.create({
+      data: {
+        name: "test-workflow",
+      },
+    });
+  }),
+
+  testAi: protectedProcedure.mutation(async () => {
+
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return { success: true, message: "Job queued" };
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
